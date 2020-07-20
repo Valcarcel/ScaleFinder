dc0ce7529819d6aaf2bf5405a75cac2c8aac9f1d
Ramon <ramon.widmer@gmail.com>
2020-07-19 19:57:19 -0700

    No syantax errors 3

--- guitar.py
+++ guitar.py
@@ -1,4 +1,8 @@
 
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.patches as mpatches
+
 # a sufficiently long list to take 12 notes out of any note
 whole_notes = ['C' , 'C#', 'D', 'D#', 'E', 'F', 'F#' , 'G', 'G#', 'A', 'A#', 'B']*3
 
@@ -32,5 +36,67 @@ for i in strings.keys():
     strings[i] = whole_notes[start:start+20]
 
 
-print('Notes in the A string: ', strings['A'])
-print('Notes in the G string: ', strings['G'])
+def find_notes(scale):
+    notes_strings = {i:0 for i in "EADGB"}
+    # for every string 
+    for key in strings.keys():
+        # we create an empty list of indexes
+        indexes = []
+        for note in scale:
+            # append index where note of the scale is found in
+            ind = strings[key].index(note)
+            indexes.append(ind)
+            # because there are 20 frets, there are duplicate notes in the string
+            if ind <= 7:
+                # we must also append these to indexes
+                indexes.append(ind+12)
+        notes_strings[key] = indexes
+    return notes_strings
+
+
+
+
+def plot(key, intervals, night=True):
+    scale = get_notes(key, intervals)
+    # Plot Strings
+    fig, ax = plt.subplots(figsize=(20,6))
+    background = ['white', 'black']
+    for i in range(1,7):
+        ax.plot([i for a in range(22)])
+    # Plotting Frets
+    for i in range(1,21):
+        if i == 12:
+            ax.axvline(x=i, color='gray', linewidth=3.5)
+            continue
+        ax.axvline(x=i, color=background[night-1], linewidth=0.5)
+    
+    #ax.grid(linestyle='-', linewidth='0.5', color='black')
+    ax.set_axisbelow(True)
+    
+    # setting height and width of displayed guitar
+    ax.set_xlim([0.5, 21])
+    ax.set_ylim([0.4, 6.5])
+    ax.set_facecolor(background[night])
+    to_plot = find_notes(scale)
+    
+    # ACHTUNG!!!
+    for y_val, key in zip([1,2,3,4,5,6], 'EADGBE'):
+        for i in to_plot[key]:
+            font = 12
+            x = i+0.5  # /figheight
+            p = mpatches.Circle((x, y_val), 0.2)
+            ax.add_patch(p)
+            note = strings[key][i]
+            # if note is root make it a bit bigger
+            if note == scale[0]:
+                font=14.5
+            ax.annotate(note, (i+0.5, y_val), color='w', weight='bold', 
+                            fontsize=font, ha='center', va='center')
+                
+    plt.title('_| _| _| _| _|'*16)
+    plt.yticks(np.arange(1,7), ['E', 'A', 'D', 'G', 'B', 'E'])
+    plt.xticks(np.arange(21)+0.5, np.arange(0,22))
+    plt.show()    
+    
+    plot('G', scales['major'])
+    
