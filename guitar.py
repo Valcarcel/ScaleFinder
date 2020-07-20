2a935149bc883c534b082bba84e170efbaabdd28
Ramon <ramon.widmer@gmail.com>
2020-07-19 19:37:23 -0700

    No syantax errors 2

--- guitar.py
+++ guitar.py
@@ -1,29 +1,36 @@
 
-def get_notes(key, intervals):
-    # a sufficiently long sequence of notes to slice from
-    whole_notes = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']*3
-    # finding start of slice
-    root = whole_notes.index(key)
-    # taking 12 consecutive elements
-    octave = whole_notes[root:root+12]
-    # accesing indexes specified by `intervals` to retrieve notes
-    return [octave[i] for i in intervals]  
+# a sufficiently long list to take 12 notes out of any note
+whole_notes = ['C' , 'C#', 'D', 'D#', 'E', 'F', 'F#' , 'G', 'G#', 'A', 'A#', 'B']*3
 
+def get_notes(key, intervals):
+    """Given any key C, C#... B
+       and intervals z.B Tone Tone Semitone"""
+    root = whole_notes.index(key)
+    octave = whole_notes[root:root+12]
+    return [octave[i] for i in intervals]
+
+# Now given some intervals
 scales = { 
     "major" : [0, 2, 4, 5, 7, 9, 11],
-    "minor" : [0, 2 , 3, 5, 7, 10, 11],
-    "dorian" : [0,  2,  3,  5,  7,  9, 10, 12],
-    "phrygian" : [0, 1, 3, 5, 7, 8, 10, 12],
+    "minor" : [0, 2 , 3, 5, 7,8, 10,],
+    "dorian" : [0,  2,  3,  5,  7,  9, 10],
+    "phrygian" : [0, 1, 3, 5, 7, 8, 10 ],
     "minor_pentatonic" : [0, 3, 5, 7, 10],
     "major_pentatonic" : [0, 2, 4, 7, 9],
-    "harmonic_minor" : [0, 2, 3, 5, 7, 8, 10, 12],
-    "mixolydian": [0, 2, 4, 5, 7, 9, 10],
+    "harmonic_minor" : [0, 2, 3, 5, 7, 8, 10,],
+    "aeolian" : [0, 2, 3, 5, 7, 8, 10,],
     "minor_blues" : [0, 3, 5, 6, 7, 10],
-    "locrian" : [0, 1, 3, 5, 6, 8, 10, 12],
-    "lydian" :[0, 2, 4, 6, 7, 9, 11, 12],
+    "locrian" : [0, 1, 3, 5, 6, 8, 10,],
+    "lydian" :[0, 2, 4, 6, 7, 9, 11,],
 }
 
-print get_notes('B', scales['minor'])
+# Creating strings of guitar
+start_strings = []
+strings = {i:0 for i in 'EADGB'}
+for i in strings.keys():
+    start = whole_notes.index(i)
+    strings[i] = whole_notes[start:start+20]
 
 
-    
+print('Notes in the A string: ', strings['A'])
+print('Notes in the G string: ', strings['G'])
